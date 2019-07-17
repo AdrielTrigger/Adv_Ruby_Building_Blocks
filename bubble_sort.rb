b0019561e3b0599ce160e3767f9ed4955b6c0ce6
Adriel Trigger <adril_trigger@yahoo.com>
2019-07-17 14:44:40 -0300

    bubble sort updated to include bubble sort by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -27,3 +27,33 @@ def bubble_sort(array)
 end
 
 bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by(array)
+    sorted = false
+
+    while sorted == false
+        i = 0
+        k = 0
+        swap = false
+        while i < array.length - (k + 1)
+            left = array[i]
+            right = array[i+1]
+            if yield(left, right) > 0
+                array[i] = right
+                array[i+1] = left
+                k += 1
+                swap = true
+            end
+            i += 1
+        end
+        if swap == false
+            sorted = true
+        end
+    end
+
+    puts array
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left, right|
+    left.length - right.length
+end
